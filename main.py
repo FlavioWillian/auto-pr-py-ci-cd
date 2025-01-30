6a7936bba70e4cbb1cc99a4e6d1c87e4a95ab860
FlavioWillian <flavio.guedes@salonline.com.br>
2025-01-30 18:41:59 -0300

    code simple for gcp

---
+++ main.py
@@ -0,0 +1,19 @@
+import functions_framework
+from flask import jsonify, request
+
+@functions_framework.http
+def hello_gcp(request):
+    """
+    Função HTTP para GCP Function.
+    Retorna uma mensagem de boas-vindas baseada no parâmetro 'name'.
+    """
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    name = "Mundo"  # Valor padrão
+    if request_json and "name" in request_json:
+        name = request_json["name"]
+    elif request_args and "name" in request_args:
+        name = request_args["name"]
+
+    return jsonify({"message": f"Olá, {name}!"}), 200
